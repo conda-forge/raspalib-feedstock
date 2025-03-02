f7372687c9ac5bc6fe17608d0ef4443e14266d27
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-02 22:48:02 +0100

    add apple silicon support

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,6 @@
 if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
   cmake -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
 else
-  cmake ${CMAKE_ARGS} -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  cmake ${CMAKE_ARGS} -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
 fi
 ninja -C build install
