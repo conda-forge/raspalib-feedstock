7a2c6d9a3a2953d51749d76aa243136d41ceda95
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-13 09:56:57 +0100

    add windows support

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-cmake ${CMAKE_ARGS} -G "Ninja Multi-Config" -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake -B build -G "Ninja Multi-Config" --preset windows_conda_raspalib -DCMAKE_INSTALL_PREFIX=%PREFIX% 
 if errorlevel 1 exit 1
 ninja -C build install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  cmake -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  if [[ "${target_platform}" == linux-64 ]]; then
+    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  elif [[ "${target_platform}" == linux-ppc64le ]]; then
+    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX} 
+  elif [[ "${target_platform}" == linux-aarch64 ]]; then
+    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  elif [[ "${target_platform}" == osx-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  fi
 else
-  cmake ${CMAKE_ARGS} -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+  if [[ "${target_platform}" == "linux"* ]]; then
+    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  elif  [[ "${target_platform}" == osx-* ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+  fi
 fi
 ninja -C build install -v
+
