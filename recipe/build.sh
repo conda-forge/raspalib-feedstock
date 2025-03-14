06261f24c4e2b040dac78d72cfa6eb669f4fe7cb
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-14 16:15:35 +0100

    attempt cross-compilation

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,16 +2,14 @@
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
   if [[ "${target_platform}" == linux-64 ]]; then
     cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
-  elif [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
-  elif [[ "${target_platform}" == linux-aarch64 ]]; then
-    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
   elif [[ "${target_platform}" == osx-* ]]; then
     cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
   fi
 else
-  if [[ "${target_platform}" == "linux"* ]]; then
-    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  if [[ "${target_platform}" == linux-ppc64le ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  elif [[ "${target_platform}" == linux-aarch64 ]]; then
+    cmake ${CMAKE_ARGS} -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
   elif  [[ "${target_platform}" == osx-* ]]; then
     cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
   fi
