07e10202b5fe16d2adfb1c8bbbb68646bc4d7950
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-05-08 01:29:01 +0200

    Update for 3.0.5 and opencl (#13)
    Co-authored-by: David Dubbeldam <d.dubbeldam@uva.nl>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,15 @@
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   if [[ "${target_platform}" == linux* ]]; then
-    cmake -B build --preset=linux_conda_raspalib
+    cmake -B build --preset=linux_conda_raspalib -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspalib
+    cmake -B build --preset=mac_conda_raspalib -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 else
   if [[ "${target_platform}" == linux* ]]; then
-    cmake -B build --preset=linux_conda_raspalib ${CMAKE_ARGS}
+    cmake -B build --preset=linux_conda_raspalib ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif  [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspalib ${CMAKE_ARGS}
+    cmake -B build --preset=mac_conda_raspalib ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 fi
 
